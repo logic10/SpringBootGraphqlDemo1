d6efea49b8119a815ab60575d04153e0969d57bf
logic10 <uthenb@gmail.com>
2018-03-23 14:40:44 +0700

    Testing

--- src/main/java/th/co/truecorp/esd/grapghql/SpringBootGraphqlApplication.java
+++ src/main/java/th/co/truecorp/esd/grapghql/SpringBootGraphqlApplication.java
@@ -14,6 +14,7 @@ public class SpringBootGraphqlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGraphqlApplication.class, args);
 		
-		//TODO: Test
+		
+		
 	}
 }
