6f2a1dab897275ef94d4603add419f92da6adfba
Usuf <tejaplanet@gmail.com>
2018-05-02 15:14:33 +0700

    Update code.

--- readme.md
+++ readme.md
@@ -1,1 +1,73 @@
+Rest.
 http://localhost:8888/swagger-ui.html
+
+Graph.
+http://localhost:8888/graphiql
+
+#Readme
+Example.
+#################################################
+#1.1 Query searchCustomer
+query searchCustomer($input: CustomerFilter!){
+
+  searchCustomerByCusttypeAndCert: searchCustomer(filter:$input){
+      customerId
+      operatorId
+      subscribers{
+         subSeqNo
+         operatorId
+      }
+  }
+}
+#Query variable
+{
+  "input" : {
+                "customerType": "I"
+               ,"certification":"3700800531726"
+           }
+}
+#######################__
+#1.2 mutation createCustomer
+mutation createCustomer($input: CustomerInput!){
+
+  createCustomer(customer:$input){
+      customerId
+      operatorId
+      subscribers{
+         subSeqNo
+         operatorId
+      }
+  } 
+}
+#Query variable
+{
+  "input" : {
+                "customerId": 84863
+               ,"operatorId": 106
+               ,"applicationId": "CD"
+           }
+}
+############################
+#1.3 mutation updateCustomer
+mutation updateCustomer($input: CustomerInput!){
+
+  createCustomer(customer:$input){
+      customerId
+      operatorId
+      subscribers{
+         subSeqNo
+         operatorId
+      }
+  }
+  
+}
+#Query variable
+{
+  "input" : {
+                "customerId": 84863
+               ,"operatorId": 106
+               ,"billCycProdFreq": 2
+               ,"applicationId": "CD"
+           }
+}
+############################
--- src/main/java/th/co/truecorp/esd/grapghql/resolver/SearchCustomerResolver.java
+++ src/main/java/th/co/truecorp/esd/grapghql/resolver/SearchCustomerResolver.java
@@ -1,6 +1,6 @@
 package th.co.truecorp.esd.grapghql.resolver;
 
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,61 +10,70 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
+import th.co.truecorp.esd.grapghql.models.CsmAccount;
 import th.co.truecorp.esd.grapghql.models.Customer;
+import th.co.truecorp.esd.grapghql.models.Subscriber;
 import th.co.truecorp.esd.grapghql.models.filter.CustomerFilter;
+import th.co.truecorp.esd.grapghql.repositories.CsmAccountRepository;
 import th.co.truecorp.esd.grapghql.repositories.CustomerRepository;
+import th.co.truecorp.esd.grapghql.repositories.SubscriberRepository;
 
 @Component
-public class SearchCustomerResolver implements DataFetcher<List<Customer>>{
+public class SearchCustomerResolver implements DataFetcher<List<Customer>> {
 
-	@Autowired
-	private CustomerRepository customerRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
-	@Autowired
-	private  ObjectMapper objectMapper = null;
+    @Autowired
+    private ObjectMapper objectMapper = null;
 
-	@Override
-	public List<Customer> get(DataFetchingEnvironment environment) {
-		//LinkedHashMap link = environment.getArgument("filter");
-		Object  rawInput = environment.getArgument("filter");
-		CustomerFilter customerinput = objectMapper.convertValue(rawInput, CustomerFilter.class);
+    @Autowired
+    SubscriberRepository subscriberRepository;
 
-		if (customerinput != null && customerinput.getCustomerId() != 0) {
-			int customer_id = customerinput.getCustomerId();
-			return customerRepository.findByCustomerId(customer_id);
-		}else if(customerinput != null && customerinput.getOperatorId() != 0) {
-			int oper_id = (Integer) customerinput.getOperatorId();
-			return customerRepository.findByOperatorId(oper_id);
-		}else if(customerinput != null
-				&& !customerinput.getCertification().equals("")
-				&& !customerinput.getCustomerType().equals("") ) {
-			String certification = customerinput.getCertification();
-			String customerType = customerinput.getCustomerType();
-			return customerRepository.findByCustomerTypeAndL9Identification(customerType,certification);
-		} else if( customerinput != null &&  !customerinput.getPrimResourceVal().equals("")) {
-			String primResourceVal = customerinput.getPrimResourceVal();
-			List<Customer> custList = new ArrayList<Customer>();
-			List<Subscriber> subList = subscriberRepository.findByPrimResourceVal(primResourceVal);
+    @Autowired
+    CsmAccountRepository csmAccountRepository;
 
-			for(int sub = 0 ; sub < subList.size() ;sub++) {
-				int cus_id  = subList.get(sub).getCustomer().getCustomerId();
-				custList.addAll(customerRepository.findByCustomerId(cus_id));
-			}
-			return custList;
-		}else if(customerinput != null && !customerinput.getBan().equals("")) {
-			String banNo = customerinput.getBan();
-			List<Customer> custList = new ArrayList<Customer>();
-			List<CsmAccount> csmBans = csmAccountRepository.findCsmAccountByBan(banNo);
+    @Override
+    public List<Customer> get(DataFetchingEnvironment environment) {
+        //LinkedHashMap link = environment.getArgument("filter");
+        Object rawInput = environment.getArgument("filter");
+        CustomerFilter customerinput = objectMapper.convertValue(rawInput, CustomerFilter.class);
 
-			for (int ban = 0; ban < csmBans.size(); ban++) {
-				int cus_id = csmBans.get(ban).getCustomer().getCustomerId();
-				custList.addAll(customerRepository.findByCustomerId(cus_id));
-			}
+        if (customerinput != null && customerinput.getCustomerId() != 0) {
+            int customer_id = customerinput.getCustomerId();
+            return customerRepository.findByCustomerId(customer_id);
+        } else if (customerinput != null && customerinput.getOperatorId() != 0) {
+            int oper_id = (Integer) customerinput.getOperatorId();
+            return customerRepository.findByOperatorId(oper_id);
+        } else if (customerinput != null
+                && !customerinput.getCertification().equals("")
+                && !customerinput.getCustomerType().equals("")) {
+            String certification = customerinput.getCertification();
+            String customerType = customerinput.getCustomerType();
+            return customerRepository.findByCustomerTypeAndL9Identification(customerType, certification);
+        } else if (customerinput != null && !customerinput.getPrimResourceVal().equals("")) {
+            String primResourceVal = customerinput.getPrimResourceVal();
+            List<Customer> custList = new ArrayList<Customer>();
+            List<Subscriber> subList = subscriberRepository.findByPrimResourceVal(primResourceVal);
 
-		}else {
-			//return customerRepository.fin
-			return null;
-		}
-	}
+            for (int sub = 0; sub < subList.size(); sub++) {
+                int cus_id = subList.get(sub).getCustomer().getCustomerId();
+                custList.addAll(customerRepository.findByCustomerId(cus_id));
+            }
+            return custList;
+        } else if (customerinput != null && !customerinput.getBan().equals("")) {
+            String banNo = customerinput.getBan();
+            List<Customer> custList = new ArrayList<Customer>();
+            List<CsmAccount> csmBans = csmAccountRepository.findCsmAccountByBan(banNo);
+
+            for (int ban = 0; ban < csmBans.size(); ban++) {
+                int cus_id = csmBans.get(ban).getCustomer().getCustomerId();
+                custList.addAll(customerRepository.findByCustomerId(cus_id));
+            }
+            return custList;
+        }
+        return null;
+
+    }
 
 }
